29e3a9895f1827898353b1df4424c8afad66ffad
bradleykenny <bkenny283@gmail.com>
2020-08-11 10:41:03 +1000

    Changes to package.json and tsconfig.json. Also, adding Prettier.

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,52 @@
-import express, {Application, Request,Response, NextFunction} from 'express';
+import express, { Application, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import mysql, { Connection } from "mysql";
-import {v4 as uuid} from 'uuid';
-import dotenv from 'dotenv';
+import { v4 as uuid } from "uuid";
+import dotenv from "dotenv";
 
-const app: Application= express();
+const app: Application = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 dotenv.config();
-var connection: Connection =mysql.createConnection({
-    host     : process.env.HOST,
-    user     : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DB
-  });
+
+var connection: Connection = mysql.createConnection({
+	host: process.env.FT_HOST,
+	user: process.env.FT_USER,
+	password: process.env.FT_PASSWORD,
+	database: process.env.FT_DB,
+});
+
 connection.connect();
 
-/*
 var favours: Array<object> = [];
 
-app.get('/favours', (req: Request,res:Response, next:NextFunction)=>{
-    const count : Number = Number(req.query["count"]); //Return number of favours requested by users
-    if(favours.length<count){
-        for (let i = favours.length; i < count; i++) {
-            favours.push({
-                "_id":"fvr_"+uuid(),
-                "user_id":"usr_"+uuid(),
-                "description":"Yo Imma do somethin for ya",
-                "location":"Favortown"
-            }); //Generate a list of favours
-            }
-    }
-    res.send(favours); 
-})
-app.post('/favours', (req: Request,res:Response, next:NextFunction)=>{
+app.get("/favours", (req: Request, res: Response, next: NextFunction) => {
+	const count: Number = Number(req.query["count"]); //Return number of favours requested by users
+	if (favours.length < count) {
+		for (let i = favours.length; i < count; i++) {
+			favours.push({
+				_id: "fvr_" + uuid(),
+				user_id: "usr_" + uuid(),
+				description: "Yo Imma do somethin for ya",
+				location: "Favortown",
+			}); //Generate a list of favours
+		}
+	}
+	res.send(favours);
+});
+app.post("/favours", (req: Request, res: Response, next: NextFunction) => {
+	favours.push({
+		_id: "fvr_" + uuid(),
+		user_id: "usr_" + uuid(),
+		description: req.body["description"],
+		location: req.body["location"],
+	}); //Generate a list of favours
+	console.log(favours[favours.length - 1]);
+	res.send(req.query);
+});
 
-    favours.push({
-        "_id":"fvr_"+uuid(),
-        "user_id":"usr_"+uuid(),
-        "description":req.body["description"],
-        "location":req.body["location"]
-    }); //Generate a list of favours
-    console.log(favours[favours.length-1])
-    res.send(req.query)
-    
-})
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+	res.send("hi");
+});
 
-app.get('/', (req: Request,res:Response, next:NextFunction)=>{
-    res.send("hi");
-})
-
-app.listen(5000, () => console.log('Server running'))
-*/
+app.listen(5000, () => console.log("Server running"));
