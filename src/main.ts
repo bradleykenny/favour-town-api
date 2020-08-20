a017c71a71f70c42de8f32ead7324eda3e5abc2b
Anthony Heng <anthony.heng29@gmail.com>
2020-08-20 15:42:32 +1000

    converting encrypted binary password from sql server to string on login

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,13 @@ import bodyParser from "body-parser";
 import mysql, { Connection } from "mysql";
 import { v4 as uuid } from "uuid";
 import dotenv from "dotenv";
-<<<<<<< Updated upstream
-import sha1 from "sha1";
-
-=======
 import bcrypt from "bcrypt";
->>>>>>> Stashed changes
 const app: Application = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-<<<<<<< Updated upstream
-=======
 const saltRounds = 10;
 
->>>>>>> Stashed changes
 dotenv.config();
 
 var sqlConn: Connection = mysql.createConnection({
@@ -27,15 +19,7 @@ var sqlConn: Connection = mysql.createConnection({
 	database: process.env.FT_DB,
 });
 
-<<<<<<< Updated upstream
-sqlConn.connect();
-
-var favours: Array<object> = [];
-
-// Return number of favours requested by users, specified by count
-=======
 //Return number of favours requested by users, specified by count
->>>>>>> Stashed changes
 app.get("/favours", (req: Request, res: Response, next: NextFunction) => {
 	const count: Number =
 		req.query["count"] != undefined ? Number(req.query["count"]) : 20; //Default to 20 if no count is given
@@ -44,18 +28,6 @@ app.get("/favours", (req: Request, res: Response, next: NextFunction) => {
 		result
 	) {
 		if (err) throw err;
-<<<<<<< Updated upstream
-		res.send(result); // Send back list of object returned by SQL query
-	});
-});
-
-// Post request to submit a favour
-// TODO: Determine valid user once login system works
-app.post("/favours", (req: Request, res: Response, next: NextFunction) => {
-	//TODO: Check user valid
-	const sqlQuery = `INSERT INTO Favour (_id, user_id, title,location,description, favour_coins) VALUES (?,?,?,?,?,?)`;
-
-=======
 		res.send(result); //Send back list of object returned by SQL query
 	});
 });
@@ -65,16 +37,11 @@ app.post("/favours", (req: Request, res: Response, next: NextFunction) => {
 	//TODO: Check user valid
 	const sqlQuery = `INSERT INTO Favour (_id, user_id, title,location,description, favour_coins) VALUES (?,?,?,?,?,?)`;
 
->>>>>>> Stashed changes
 	sqlConn.query(
 		sqlQuery,
 		[
 			"fvr_" + uuid(),
-<<<<<<< Updated upstream
-			req.body["user_id"], // Replace with req.body["user_id"] when login is ready
-=======
 			req.body["user_id"], //Replace with session id when sessions is ready
->>>>>>> Stashed changes
 			req.body["title"],
 			req.body["location"],
 			req.body["description"],
@@ -88,15 +55,9 @@ app.post("/favours", (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.post("/login", (req: Request, res: Response, next: NextFunction) => {
-<<<<<<< Updated upstream
-	// TODO: After sprint 1 add the capability to login with email as well
-	const sqlQuery: string =
-		"SELECT _id, username, password FROM User WHERE username=?"; // NOTE: can we compare unencrypted password to sql encrypted password?
-=======
 	//TODO: After sprint 1 add the capability to login with email as well
 	const sqlQuery: string =
 		"SELECT _id, username, password FROM User WHERE username=?"; //NOTE: can we compare unencrypted password to sql encrypted password?
->>>>>>> Stashed changes
 	sqlConn.query(sqlQuery, [req.body["username"]], function (err, result) {
 		if (err) throw err;
 		if (result.length != 1) {
@@ -104,82 +65,30 @@ app.post("/login", (req: Request, res: Response, next: NextFunction) => {
 			console.log("failed login, sql return:", result);
 			return;
 		}
-<<<<<<< Updated upstream
-		if (sha1(req.body["password"]) != result[0]["password"]) {
-			console.log(
-				"Wrong password attempt:",
-				result[0]["password"],
-				req.body["password"]
-			);
-			res.send("ERROR: Login failed");
-			return;
-		}
 
-		console.log("login:", req.body);
-		res.send(result[0]["_id"]); // Send back OK if successfully inserted
-=======
-
-		bcrypt.compare(req.body["password"], result[0]["password"], function (
-			err,
-			correct
-		) {
-			if (correct) {
-				console.log("login:", req.body);
-				res.send(result[0]["_id"]); // Send back OK if successfully inserted
-			} else {
-				console.log(
-					"Wrong password attempt:",
-					result[0]["password"],
-					req.body["password"]
-				);
-				res.send("ERROR: Login failed");
+		bcrypt.compare(
+			req.body["password"],
+			result[0]["password"].toString(), //SQL server returns binary string, need to convert to regular string to compare first
+			function (err, correct) {
+				if (correct) {
+					console.log("login:", req.body);
+					res.send(result[0]["_id"]); // Send back OK if successfully inserted
+				} else {
+					console.log(
+						"Wrong password attempt:",
+						result[0]["password"],
+						req.body["password"]
+					);
+					res.send("ERROR: Login failed");
+				}
 			}
-		});
->>>>>>> Stashed changes
+		);
 	});
 });
 
 app.post("/register", (req: Request, res: Response, next: NextFunction) => {
 	//Check user valid
 	const sqlQuery: string = `INSERT INTO User (_id, username, password,email_addr, favour_counter) VALUES (?,?,?,?,?)`;
-<<<<<<< Updated upstream
-
-	sqlConn.query(
-		sqlQuery,
-		[
-			"usr_" + uuid(),
-			req.body["username"],
-			req.body["password"],
-			req.body["email"],
-			0,
-		],
-		function (err, result) {
-			if (err) {
-				switch (err.code) {
-					case "ER_DUP_ENTRY":
-						const msg: any = err.sqlMessage;
-						if (msg.endsWith("'username'")) {
-							res.send("ERROR: Username already taken");
-						} else if (msg.endsWith("'email_addr'")) {
-							res.send(
-								"ERROR: Account with email already exists"
-							);
-						} else {
-							res.send("ERROR: Unknown Login Error");
-						}
-						break;
-					default:
-						res.send(err.sqlMessage); // Just send sql error message
-						break;
-				}
-				return;
-			}
-			console.log(result);
-			//TODO: Send back username/email already exists error if it exists. Check if SQL will send back this response once _id, username and email has been made unique
-			res.send("OK"); // Send back OK if successfully registered
-		}
-	);
-=======
 	bcrypt.hash(req.body["password"], saltRounds, function (err, hash) {
 		//Need to throw error on bad hash?
 		sqlConn.query(
@@ -212,7 +121,6 @@ app.post("/register", (req: Request, res: Response, next: NextFunction) => {
 			}
 		);
 	});
->>>>>>> Stashed changes
 });
 
 app.listen(5000, () => console.log("Server running"));
