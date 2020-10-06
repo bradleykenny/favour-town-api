7938c89f19b76efa13b3862d6ef415c404d5077f
John K <Callidusjohn@users.noreply.github.com>
2020-10-06 21:04:07 +1100

    not finsing the server url?

---
+++ src/upload.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const AWS = require("aws-sdk");
+
+// uploading functionalities
+const uploadFile = (fileName) => {
+	// Read content from the file
+	const fileContent = fs.readFileSync(fileName);
+
+	// Setting up S3 upload parameters
+	const params = {
+		Bucket: BUCKET_NAME,
+		Key: "cat.jpg", // File name you want to save as in S3
+		Body: fileContent,
+	};
+
+	// Uploading files to the bucket
+	s3.upload(params, (err, data) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`File uploaded successfully. ${data.Location}`);
+	});
+};
+
+/************* Client Initialisation *************/
+// Enter copied or downloaded access ID and secret key here
+// for user "Test"
+const ID = "AKIAVLL6HVDOX2VU4H5S";
+const SECRET = "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";
+
+// The name of the bucket that you have created
+const BUCKET_NAME = "favourtown";
+
+// credentials
+const s3 = new AWS.S3({
+	accessKeyId: ID,
+	secretAccessKey: SECRET,
+});
--- src/upload.ts
+++ src/upload.ts
@@ -1,31 +1,36 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
+const fs = require("fs");
+const AWS = require("aws-sdk");
 
 /************* Client Initialisation *************/
 // Enter copied or downloaded access ID and secret key here
 // for user "Test"
-const ID = 'AKIAVLL6HVDOX2VU4H5S';
-const SECRET = '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';
+const ID = "AKIAVLL6HVDOX2VU4H5S";
+const SECRET = "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";
 
 // The name of the bucket that you have created
-const BUCKET_NAME = 'favourtown';
+const BUCKET_NAME = "favourtown";
 
-const uploadFile = (fileName) => {
-    // Read content from the file
-    const fileContent = fs.readFileSync(fileName);
+const s3 = new AWS.S3({
+	accessKeyId: ID,
+	secretAccessKey: SECRET,
+});
 
-    // Setting up S3 upload parameters
-    const params = {
-        Bucket: BUCKET_NAME,
-        Key: 'cat.jpg', // File name you want to save as in S3
-        Body: fileContent
-    };
+const uploadFile = (fileName: any) => {
+	// Read content from the file
+	const fileContent = fs.readFileSync(fileName);
 
-    // Uploading files to the bucket
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
+	// Setting up S3 upload parameters
+	const params = {
+		Bucket: BUCKET_NAME,
+		Key: "cat.jpg", // File name you want to save as in S3
+		Body: fileContent,
+	};
+
+	// Uploading files to the bucket
+	s3.upload(params, (err: any, data: any) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`File uploaded successfully. ${data.Location}`);
+	});
 };
