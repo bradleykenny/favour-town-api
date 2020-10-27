6b6c714e5b9e37fb117dc6bde944afd1e495876e
rundumnurd <anthony.heng@students.mq.edu.au>
2020-10-27 17:56:52 +1100

    Fixed typo 'reciever'

--- src/messenger.ts
+++ src/messenger.ts
@@ -8,7 +8,6 @@ var db: Connection = require("./db").db;
 module.exports = function (client: socketIO.Socket) {
 	if (client.handshake.session!.user_id) {
 		connectedClients[client.handshake.session!.user_id] = client;
-		console.log(client.handshake.session!.user_id)
 		client.emit("yourUser_id",client.handshake.session!.user_id)
 		db.query(
 			`SELECT friends.* FROM (SELECT u.username,msg.* FROM User u JOIN (SELECT m.* FROM Messages m WHERE m.receiver_id=? ORDER BY m.date) AS msg ON u._id=msg.sender_id 
@@ -28,8 +27,8 @@ module.exports = function (client: socketIO.Socket) {
 		//Define all socket.io events here
 		client.on("send", (data) => {
 			//Should send the receive and the message itself
-			if (connectedClients.hasOwnProperty(data["receiver"])) {
-				connectedClients[data["receiver"]].emit("incoming", [
+			if (connectedClients.hasOwnProperty(data["reciever"])) {
+				connectedClients[data["reciever"]].emit("incoming", [
 					{
 						sender_id: client.handshake.session!.user_id,
 						content: data["message"],
@@ -40,12 +39,12 @@ module.exports = function (client: socketIO.Socket) {
 				"INSERT INTO Messages (sender_id,receiver_id, content,date) VALUES (?,?,?,NOW())", //REMEMBER TO PUT THIS IN THE MYSQL DATABASE
 				[
 					client.handshake.session!.user_id,
-					data["receiver"],
+					data["reciever"],
 					data["message"],
 				],
 				function (err, result) {
 					if (err) console.log(err);
-					else client.emit("ACK", "got msg for " + data["receiver"]);
+					else client.emit("ACK", "got msg for " + data["reciever"]);
 				}
 			);
 			//Store message in db
@@ -57,7 +56,7 @@ module.exports = function (client: socketIO.Socket) {
 				"SELECT sender_id,content,date FROM Messages WHERE sender_id=? ORDER BY date LIMIT ?,10", //REMEMBER TO PUT THIS IN THE MYSQL DATABASE
 				[data["sender"], data["loaded"]],
 				function (err, result) {
-					if (err) throw err;
+					if (err) console.log(err);
 					client.emit("incoming", result);
 				}
 			);
